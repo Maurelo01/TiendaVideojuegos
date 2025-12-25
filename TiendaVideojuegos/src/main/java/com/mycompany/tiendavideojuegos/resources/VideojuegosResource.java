3587bc76ac2e36a752fec4d54ab1f29f35f9aaf4
maurelo860@gmail.com <maurelo860@gmail.com>
2025-12-24 23:47:58 -0600

    Se añadio el put para editar Juegos

--- TiendaVideojuegos/src/main/java/com/mycompany/tiendavideojuegos/resources/VideojuegosResource.java
+++ TiendaVideojuegos/src/main/java/com/mycompany/tiendavideojuegos/resources/VideojuegosResource.java
@@ -5,6 +5,7 @@ import com.mycompany.tiendavideojuegos.services.VideojuegosService;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -66,7 +67,7 @@ public class VideojuegosResource
         }
     }
     
-    @POST
+    @PUT
     @Path("{id}") // /api/videojuegos/{id}
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
